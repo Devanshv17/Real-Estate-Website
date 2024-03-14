8f24293a4f1751958cf77692dec6d48b0f379af0
thelatephylosopher <riyakashive@gmail.com>
2024-03-14 23:36:39 +0530

    UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Navigation } from 'swiper/modules';
 import SwiperCore from 'swiper';
 import 'swiper/css/bundle';
 import ListingItem from '../components/ListingItem';
+import Image from '../images/pic1restox.jpeg';
 
 export default function Home() {
   const [offerListings, setOfferListings] = useState([]);
@@ -46,27 +47,35 @@ export default function Home() {
     fetchOfferListings();
   }, []);
   return (
-    <div>
+    <div className='bg-sky-200'>
       {/* top */}
+      <div className='flex flex-row'>
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
-        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
+        <h1 className='text-neutral-700 font-bold text-3xl lg:text-6xl'>
+          Find your next <span className='text-sky-800'>perfect</span>
           <br />
           place with ease
         </h1>
-        <div className='text-gray-400 text-xs sm:text-sm'>
+       <br /> 
+        <div className='text-sky-800 text-sm sm:text-sm'>
           RESTOX. is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
+          <br />
         </div>
         <Link
           to={'/search'}
-          className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'
+          className='text-sm sm:text-sm text-sky-800 font-bold hover:underline'
         >
           Let's get started...
         </Link>
+
       </div>
+      <div className='py-32 px-5'>
+        <img src={Image} className="ml-auto"/>
+        </div>
+        </div>
 
       {/* swiper */}
       <Swiper navigation>
@@ -93,7 +102,7 @@ export default function Home() {
           <div className=''>
             <div className='my-3'>
               <h2 className='text-2xl font-semibold text-slate-600'>Recent offers</h2>
-              <Link className='text-sm text-blue-800 hover:underline' to={'/search?offer=true'}>Show more offers</Link>
+              <Link className='text-sm text-blue-800 font-bold hover:underline' to={'/search?offer=true'}>Show more offers</Link>
             </div>
             <div className='flex flex-wrap gap-4'>
               {offerListings.map((listing) => (
@@ -105,8 +114,8 @@ export default function Home() {
         {rentListings && rentListings.length > 0 && (
           <div className=''>
             <div className='my-3'>
-              <h2 className='text-2xl font-semibold text-slate-600'>Recent places for rent</h2>
-              <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=rent'}>Show more places for rent</Link>
+              <h2 className='text-2xl font-semibold text-sky-800'>Recent places for rent</h2>
+              <Link className='text-sm text-sky-800 hover:underline' to={'/search?type=rent'}>Show more places for rent</Link>
             </div>
             <div className='flex flex-wrap gap-4'>
               {rentListings.map((listing) => (
@@ -118,8 +127,8 @@ export default function Home() {
         {saleListings && saleListings.length > 0 && (
           <div className=''>
             <div className='my-3'>
-              <h2 className='text-2xl font-semibold text-slate-600'>Recent places for sale</h2>
-              <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=sale'}>Show more places for sale</Link>
+              <h2 className='text-2xl font-semibold text-sky-800'>Recent places for sale</h2>
+              <Link className='text-sm text-sky-800 font-bold hover:underline' to={'/search?type=sale'}>Show more places for sale</Link>
             </div>
             <div className='flex flex-wrap gap-4'>
               {saleListings.map((listing) => (
